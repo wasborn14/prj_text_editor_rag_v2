fb7b8cffc6ac1d703b0f6616037285802763fd9e
estyle-0170 <archibald.ff14@gmail.com>
2025-10-25 11:04:40 +0900

    fix warning

--- frontend/src/components/layout/dashboard/editor/EditorContentWrapper.tsx
+++ frontend/src/components/layout/dashboard/editor/EditorContentWrapper.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useEditor } from 'novel'
 import { SlashCommandMenu } from './SlashCommandMenu'
 import { TableToolbar } from './TableToolbar'
